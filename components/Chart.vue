d27ce819ffb73ad6c59827aa3b66ec95beb3ad65
puly garcia <pulygarcia09@gmail.com>
2024-09-16 21:58:40 -0300

    showing API data in the chart

--- components/Chart.vue
+++ components/Chart.vue
@@ -17,12 +17,7 @@
 
   const chartOptions = {
     responsive: true,
-    backgroundColor: [
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-    ]
+    backgroundColor:'rgba(54, 162, 235, 0.2)'
   }
 
   const months: string[] = [
@@ -30,13 +25,51 @@
     'July', 'August', 'September', 'October', 'November', 'December'
   ];
 
-  const chartData: ChartData = {
+  // Define as reactive reference
+  const chartData = ref<ChartData>({
     labels: months,
     datasets: [{ 
-        data: [230, 160, 362, 215, 224, 230, 160, 362, 215, 224, 215, 224] , //Sales per month (example)
-        label: 'Sales'
-    }],
-  }
+      data: Array(12).fill(0), // initialize with 0's
+      label: 'Sales'
+    }]
+  });
+
+  //process sells by each month
+  const processSalesData = (sales: any) => {
+    // array with 12 positions, one for each month
+    const monthlySales = Array(12).fill(0);
+
+    sales.forEach((sale: any) => {
+      const saleDate = new Date(sale.date); // format date
+      const month = saleDate.getUTCMonth(); // Get each month index
+      monthlySales[month] += 1; // Increment sell's counter
+    });
+
+    // update chart data
+    chartData.value = {
+      ...chartData.value,
+      datasets: [{
+        ...chartData.value.datasets[0],
+        data: monthlySales
+      }]
+    };
+    //console.log(chartData.value.datasets[0].data);
+  };
+
+  const fetchSalesData = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/api/tickets');
+      const data = await response.json()
+      processSalesData(data);
+    } catch (error) {
+      console.error('Error fetching sales data:', error);
+    }
+  };
+
+
+  onMounted(() => {
+    fetchSalesData();
+  });
 </script>
 
 <template>
